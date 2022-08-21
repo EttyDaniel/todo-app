46ac702f6c2446b9df086ddbac98af4caa0ddd97
EttyDaniel <danieleti@gmail.com>
2022-08-20 21:46:28 -0400

    adding background img

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App(props) {
 
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1>Todo-App</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
         <FilterButton/>
